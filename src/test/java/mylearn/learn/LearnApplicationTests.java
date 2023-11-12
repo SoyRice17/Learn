992ad7091d854dae5515e706c30fc6d5521661ac
SoyRice <rkswkdqkq6974@gmail.com>
2023-11-12 19:37:18 +0900

    none

---
+++ src/test/java/mylearn/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package mylearn.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
